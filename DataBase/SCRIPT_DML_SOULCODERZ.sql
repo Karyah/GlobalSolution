a0bd77222ff4394c5b0f949dd58c3223c3cca914
Karen Antiqueira <mastrogiacomokaren@gmail.com>
2022-11-03 12:24:11 -0300

    DML do usuario 10 linhas

---
+++ DataBase/SCRIPT_DML_SOULCODERZ.sql
@@ -0,0 +1,19 @@
+insert into Usuario (login, nome, email, senha) values('kaka12','Karen Antiqueira','karenantique@gmail.com','kkkaren241');
+
+insert into Usuario (login, nome, email, senha) values('akko','Davi Silva Souza','silvaSouza@yahoo.com.br','po8912rta');
+
+insert into Usuario (login, nome, email, senha) values('luffy','Carlos Antônio','dluffycarlos@bol.com','onepice987');
+
+insert into Usuario (login, nome, email, senha) values('vanesSA','Vanessa Laranjeiras','vanvan@gmail.com','Chocolate1.');
+
+insert into Usuario (login, nome, email, senha) values('Megumi','Karina Megumi Sakamoto','karimegumi@gmail.com','MEgumiii');
+
+insert into Usuario (login, nome, email, senha) values('Fina','Josefina Matos','matosjosefina@yahoo.com','12042008J.');
+    
+insert into Usuario (login, nome, email, senha) values('mariahhh','Maria José','marijose@gmail.com','judelsoi123');
+
+insert into Usuario (login, nome, email, senha) values('Carlinha','Carla Barros Borges','ACarlinha@outlook.com','odeioBarros12');
+
+insert into Usuario (login, nome, email, senha) values('OCardoso','Pedro Cardoso Campos','cardoso@gmail.com','ped342cardoss');
+
+insert into Usuario (login, nome, email, senha) values('Fernandas2','Fernanda Freitas Fernandas','FFFernanda@yahoo.com.br','fefrefe123');
